bdeb0ce6c95af759c8d4468ffe30765728a4d17a
m1kron <piotr.kowalczyk102+github@gmail.com>
2017-06-29 11:11:12 +0200

    [sts]: [SimpleTaskSystem]: added some more comments.

--- libs/simpleTaskSystem/include/globalApi.h
+++ libs/simpleTaskSystem/include/globalApi.h
@@ -2,6 +2,9 @@
 #include "iTaskSystem.h"
 #include "stsExportApi.h"
 
+// Creates task system. 
+// [TODO]: add user-define alloc function.
 extern "C" STS_API sts::ITaskSystem* CreateTaskSystem();
 
+// Deallocates task system. All handles becames invalid.
 extern "C" STS_API void DestroyTaskSystem( sts::ITaskSystem* system );
